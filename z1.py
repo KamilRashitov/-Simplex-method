f66a9a4f3dcaee70103cf676b91fdafcfdf12f6d
secretanry <75036208+secretanry@users.noreply.github.com>
2023-10-03 20:37:05 +0300

    Add files via upload

---
+++ z1.py
@@ -0,0 +1,21 @@
+print("Enter the coefficients of the main problem on one line with space delimiter")
+coeff_main = list(map(int, input().split()))
+print("Enter number of constraints")
+n = int(input())
+matrix = []
+for i in range(n):
+    matrix.append(list(map(int, input("Enter coefficients of " + str(i + 1) + "th constraint with space delimiter")
+                           .split(' '))))
+print("Enter the right-hand coefficients of the constraints on one line with space delimiter")
+coeff_constr = list(map(int, input().split()))
+print("Enter 1 if your problem is maximize and -1 if your problem is minimize")
+k = int(input())
+for i in range(len(coeff_main)):
+    coeff_main = coeff_main * k
+
+
+
+
+
+
+
